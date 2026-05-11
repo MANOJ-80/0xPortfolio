603008fdd47f5363d2c3b4dc434f71517dd51ea8
MANOJ-80 <manoj.xakep@gmail.com>
2026-05-11 13:34:15 +0530

    Remove app-level domain redirect

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.manojganesan.dev",
-          },
-        ],
-        destination: "https://manojganesan.dev/:path*",
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
